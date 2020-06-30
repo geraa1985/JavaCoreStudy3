849bb40b28d7971b4c4e2f73de5a5ac85b796908
geraa1985 <geraa1985@gmail.com>
2020-06-30 22:58:54 +0300

    Add a task about Java.concurrent

---
+++ src/Lesson_5/MainClass.java
@@ -0,0 +1,46 @@
+package Lesson_5;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+
+public class MainClass {
+    public static final int CARS_COUNT = 4;
+
+    public static CyclicBarrier cbReady = new CyclicBarrier(CARS_COUNT);
+    public static final CountDownLatch cdlReady = new CountDownLatch(CARS_COUNT);
+    public static final CountDownLatch cdlFinish = new CountDownLatch(CARS_COUNT);
+
+    public static void main(String[] args) {
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+        Car[] cars = new Car[CARS_COUNT];
+        for (int i = 0; i < cars.length; i++) {
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+        }
+        for (Car car : cars) {
+            new Thread(car).start();
+        }
+
+        try {
+            cdlReady.await();
+            System.out.println("\nВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!\n");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+        try {
+            cdlFinish.await();
+            System.out.println("\nВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+            System.out.println("Победу одержал: " + Car.getWinner());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
+
+
+
+
+
